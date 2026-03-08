3eaff4d57d3c5c36c78d64548cf4d8d90fa53a22
Renato <renatojosesfc@gmail.com>
2026-03-08 19:44:58 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// animação suave ao rolar
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+anchor.addEventListener("click", function(e){
+
+e.preventDefault()
+
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+
+behavior:"smooth"
+
+})
+
+})
+
+})
